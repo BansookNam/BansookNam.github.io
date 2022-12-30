aabf2d8895ee81d15064a76c1bbbc6bafe6d783f
Bansook Nam <bs.nam@lawfully.com>
2022-12-30 22:47:02 +0900

    build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "deaf3824ee89d08bda0a2e1ec75238a5",
-"index.html": "fd173eeb874c6a03602e9dc591de89cc",
-"/": "fd173eeb874c6a03602e9dc591de89cc",
+"index.html": "58b798e00c0969ad1099f9b6ed0e99d8",
+"/": "58b798e00c0969ad1099f9b6ed0e99d8",
 "main.dart.js": "be64f40bfcbe863904d8560ac711f52c",
+"img/bansook.png": "0cb28e16414e7606e31017809c92ae9f",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
